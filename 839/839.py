7a22c6f1fca24827434528952bda98cccb6c6c63
P4CIFIC <malek_123_chawki@hotmail.com>
2021-09-25 02:03:46 +0200

    new idea

--- 839/839.py
+++ 839/839.py
@@ -1,55 +1,29 @@
 numOfTests = int(input())
 print("\n")
-wL = ""
-dL = ""
-wR = ""
-dR = ""
-array = []
-dict = {}
+leftSides = []
+rightSides = []
 tempStr = []
-counter = 1
+areEqual = False
+offsetRightSides = 0
 
-userInput = input()
-tempStr = ""
-for line in userInput:
-    if line.isdigit():
-        tempStr += line
-    
-dict["L"+str(counter)] = tempStr[0:2]
-dict["R"+str(counter)] = tempStr[2:4]
+while numOfTests != 0:
+    while(leftSideForce and rightSideForce != 0 ):
+        userInput = input()
+        tempStr = ""
+        for line in userInput:
+            if line.isdigit():
+                tempStr += line
 
-print(dict["L" + str(counter)][0])
-print(dict["R" + str(counter)][0])
+    numOfTests -= 1
 
-while(1):
-    userInput = input()
-    tempStr = ""
-    for line in userInput:
-        if line.isdigit():
-            tempStr += line
-    counter += 1
-    dict["L"+str(counter)] = tempStr[0:2]
-    dict["R"+str(counter)] = tempStr[2:4]
-    
-    if (tempStr[0] and tempStr[2]) == 0:
-        addLeft()
-        addRight()
-    elif tempStr[0] == 0:
-        addLeft()
-    elif tempStr[2] == 0:
-        addRight()
+def checkIfEqual (string):
+    leftSideWeight = string[0]
+    leftSideLength = string[1]
+    rightSideWeight = string[2]
+    rightSideLength = string[3]
+
+    if leftSideWeight:
+        return int(startNode[0]) *  
     else:
         
-
-    #if (dict["L" + str(counter)][0] or dict["R" + str(counter)][0]) == '0':
-
-    
-print(array)
-
-def addRight():
-    if (tempStr[0]) == 0:
-        n = counter
-        n += 1
-
-def addLeft():
-    
+        
---
+++ 839/test.py
@@ -0,0 +1,5 @@
+array = []
+for i in range(1,12):
+    array.append(str(i) + " ")
+
+print(array[1])
