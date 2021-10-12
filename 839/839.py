f2f45430cdc2dc35d511733f640a86f00fd8b273
P4CIFIC <malek_123_chawki@hotmail.com>
2021-10-12 16:32:58 +0200

    update

--- 839/839.py
+++ 839/839.py
@@ -1,46 +1,40 @@
-tempStr = ""
-numOfTests = int(input())
-print("\n")
-
-
 def checkIfEqual (string):
-
-    arr = string.split(" ")
-    leftSideWeight = int(arr[0])
-    leftSideLength = int(arr[1])
-    rightSideWeight = int(arr[2])
-    rightSideLength = int(arr[3])
-    
-    leftForce = leftSideWeight * leftSideLength 
-    rightForce = rightSideWeight * rightSideLength
-    
-    if leftSideWeight == 0:
-        leftSideWeight = checkIfEqual(input())
-    
-    if rightSideWeight == 0:
-        rightSideWeight = checkIfEqual(input())
-    
-    if (leftForce == rightForce) and (leftSideWeight != 0) and (rightSideWeight != 0):
-        return leftSideWeight + rightSideWeight
-    
+    if string == "":
+        string = checkIfEqual(input())
     else:
-        return 0
+        arr = string.split(" ")
+        leftSideWeight = int(arr[0])
+        leftSideLength = int(arr[1])
+        rightSideWeight = int(arr[2])
+        rightSideLength = int(arr[3])
+        
+        leftForce = leftSideWeight * leftSideLength 
+        rightForce = rightSideWeight * rightSideLength
+        
+        if leftSideWeight == 0:
+            leftSideWeight = checkIfEqual(input())
+        
+        if rightSideWeight == 0:
+            rightSideWeight = checkIfEqual(input())
+        
+        if (leftForce == rightForce) and (leftSideWeight != 0) and (rightSideWeight != 0):
+            return leftSideWeight + rightSideWeight
+        
+        else:
+            return 0
 
+numOfTests = int(input())
+#print("\n")
 
 while numOfTests != 0:
-
-    answer = False
+    answer = ""
+    
     while(answer is False):
-        userInput = input()
-        if userInput == "":
-            continue
-        else:
-            tempStr = ""
-            answer = checkIfEqual(userInput)
-            tempStr = ""
+        answer = checkIfEqual(input())
+
+        if answer == 0:
+            
 
     print(str(answer) + "\n")
     answer = False
     numOfTests -= 1
-
-print(tempStr)
--- 839/test.py
+++
@@ -1,5 +0,0 @@
-array = []
-for i in range(1,12):
-    array.append(str(i) + " ")
-
-print(array[1])
