a8210380b85db2510d01fb9633b16a737cefd0aa
P4CIFIC <malek_123_chawki@hotmail.com>
2021-12-20 11:35:01 +0100

    update

--- problems/6_10307/10307.py
+++ problems/6_10307/10307.py
@@ -1,38 +1,67 @@
-import collections
+import sys, collections
 
+class PrimSolution():
 
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]for row in range(vertices)]
+
+    def get_mst_sum(self, parent):
+        sum = 0
+        for i in range(1, self.V):
+            sum += self.graph[i][ parent[i] ]
+        return sum
+    
+    def minKey(self, key, mstSet):
+        min = sys.maxsize
+
+        for v in range(self.V):
+            if key[v] < min and mstSet[v] == False:
+                min = key[v]
+                min_index = v
+        return min_index
+
+    def primMST(self):
+
+        key = [sys.maxsize] * self.V
+        parent = [None] * self.V 
+        key[0] = 0
+        mstSet = [False] * self.V
+        parent[0] = -1 
+
+        for cout in range(self.V):
+            u = self.minKey(key, mstSet)
+            mstSet[u] = True
+            for v in range(self.V):
+
+                if self.graph[u][v] > 0 and mstSet[v] == False and key[v] > self.graph[u][v]:
+                    key[v] = self.graph[u][v]
+                    parent[v] = u
+        return self.get_mst_sum(parent)
+        
+  
 class Graph():
-
-    # Constructs the class with an arbitrary amount of vertices
+    
     def __init__(self, vertices_no) -> None:
         self.vertices_no = vertices_no
-        # driver code
         self.graph = collections.OrderedDict()
     
-    # Add a vertex to the dictionary
     def add_vertex(self, v):
 
         if v not in self.graph:
             self.vertices_no = self.vertices_no + 1
             self.graph[v] = []
 
-    # Add an edge between vertex v1 and v2 with edge weight e
     def add_edge(self, v1, v2, e):
 
-        # Check if vertex v1 is a valid vertex
         if v1 not in self.graph:
             print("Vertex ", v1, " does not exist.")
-        # Check if vertex v2 is a valid vertex
         elif v2 not in self.graph:
             print("Vertex ", v2, " does not exist.")
         else:
-            # Since this code is not restricted to a directed or 
-            # an undirected graph, an edge between v1 v2 does not
-            # imply that an edge exists between v2 and v1
             temp = [v2, e]
             self.graph[v1].append(temp)
-                
-    # Print the graph
+        
     def print_graph(self):
         for vertex in self.graph:
             for edges in self.graph[vertex]:
@@ -67,7 +96,7 @@ class ShortestPath():
         self.list_of_nodes = []
         
         self.set_dimensions()
-        
+
         counter_num_lines = 0
         while counter_num_lines != self.height:
             temp = [element for element in input()]
@@ -84,9 +113,8 @@ class ShortestPath():
                     renaming_counter += 1
         self.list_of_nodes.insert(0, self.start)
         
-        print(self.matrix)
-        print(self.list_of_nodes)            
-                    
+        """print(self.matrix)
+        print(self.list_of_nodes)  """          
     
     def bfs(self, start, goal):
         start = self.get_position(start)
@@ -102,25 +130,45 @@ class ShortestPath():
                     queue.append(path + [(x2, y2)])
                     seen.add((x2, y2))
     
-    def create_graph(self):
+    def create_adjacency_list(self):
         self.weighted_graph = Graph(0)
         
         for element in self.list_of_nodes:
-            self.weighted_graph.add_vertex(element)        
+            self.weighted_graph.add_vertex(element)
         
         for i in self.list_of_nodes:
             for j in self.list_of_nodes:
                 if i != j:
+                    # -1 at cost to compensate for bfs misscalculation
                     self.weighted_graph.add_edge(i,j, len(self.bfs(i,j)) - 1)
         
-        self.weighted_graph.print_graph() 
-
+        #self.weighted_graph.print_graph()
+    
+    def create_adjacency_matrix(self):
+        self.prims_algo = PrimSolution(len(self.list_of_nodes))
+        matrix = self.prims_algo.graph
+        weighted_graph = list(self.weighted_graph.graph.items())
+        
+        for node in weighted_graph:
+            for weight in node[1]:
+                matrix[self.list_of_nodes.index(weight[0])][self.list_of_nodes.index(node[0])] = weight[1]
+        
+        self.prims_algo.graph = matrix
+        sum = self.prims_algo.primMST()
+        return sum
+  
 
 number_of_test_cases = int(input())
+sums = []
 
 while number_of_test_cases > 0:
     sp = ShortestPath("S","A", "#", input())
     sp.create_matrix()
-    sp.create_graph()
+    sp.create_adjacency_list()
+    sum = sp.create_adjacency_matrix()
+    sums.append(sum)
     # decrements numbers of test cases
     number_of_test_cases -= 1
+
+for sum in sums:
+    print(sum)
--- problems/6_10307/prims_msp.py
+++ problems/6_10307/prims_msp.py
@@ -12,9 +12,9 @@ class Graph():
 
 	# A utility function to print the constructed MST stored in parent[]
 	def printMST(self, parent):
-		print "Edge \tWeight"
+		print ("Edge \tWeight")
 		for i in range(1, self.V):
-			print parent[i], "-", i, "\t", self.graph[i][ parent[i] ]
+			print (parent[i], "-", i, "\t", self.graph[i][ parent[i] ])
 
 	# A utility function to find the vertex with
 	# minimum distance value, from the set of vertices
@@ -75,8 +75,6 @@ g.graph = [ [0, 2, 0, 6, 0],
 			[2, 0, 3, 8, 5],
 			[0, 3, 0, 0, 7],
 			[6, 8, 0, 0, 9],
-			[0, 5, 7, 9, 0]]
+			[0, 5, 7, 9, 0] ]
 
-g.primMST();
-
-# Contributed by Divyanshu Mehta
+g.primMST()
