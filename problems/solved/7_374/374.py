113d48c6f91c78f23c5180823c5c65fb98944a06
P4CIFIC <malek_123_chawki@hotmail.com>
2021-12-21 03:57:32 +0100

    update

---
+++ problems/solved/7_374/374.py
@@ -0,0 +1,27 @@
+from sys import stdin
+calc_data = []
+
+def calculate(b,p,m):
+    return pow(int(b),int(p),int(m))
+
+def process(line):
+    #temp = line
+    calc_data.append(line)
+    if len(calc_data) == 3:
+        print(calculate(calc_data[0], calc_data[1], calc_data[2]))
+        calc_data.clear()
+
+for line in stdin:
+    if line == '':
+        """
+        for result in calc_results:
+            print(result)"""
+        break
+    try:
+        line = int(line)
+        process(int(line))
+    except ValueError:
+        continue
+        
+        
+
