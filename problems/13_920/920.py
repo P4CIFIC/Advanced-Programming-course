cfe42ccae9341115c82250b44c42e85198522273
P4CIFIC <malek_123_chawki@hotmail.com>
2022-01-19 18:47:12 +0100

    finished 920

--- problems/13_920/920.py
+++ problems/13_920/920.py
@@ -5,9 +5,6 @@ class Point():
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
-    #For debugging 
-    def __repr__(self) -> str:
-        return (f"x:{self.x} y:{self.y}")
 
 #returns the variables k and m from "y = kx + m"
 def get_lin_equation(point_a, point_b):
@@ -31,7 +28,8 @@ def find_intersections(coordinates, n):
             if coordinates[i].y > peak_1.y:
                 peak_2 = peak_1
                 peak_1 = coordinates[i]
-                
+                if peak_2.y == 0:
+                    distance += get_distance(peak_1, peak_2)
                 if peak_2.y != 0:
                     peak_1_friend = coordinates[i-1]
                     k, m = get_lin_equation(peak_1, peak_1_friend)
@@ -39,13 +37,12 @@ def find_intersections(coordinates, n):
                     distance += math.hypot(peak_1.x - x, peak_1.y - peak_2.y)
         except IndexError:
             break
-    print(distance)
+    print("{0:.2f}".format(distance))
     
 # Reading the dataset and creating Points, assigning 'x' and 'y' to the 
 # instance variables of the different Point objects.   
 number_of_test_cases = int(input())
 for n in range(number_of_test_cases):
-    print(f"Testcase: {n+1}")
     coordinates = []            # list for all points
     number_of_points = int(input())
     for i in range (number_of_points):
@@ -53,10 +50,7 @@ for n in range(number_of_test_cases):
         point = Point(int(temp[0]), int(temp[1]))
         coordinates.append(point)
         number_of_points = len(coordinates)
+        
     # Sorting the points in list (coordinates) by 'x' value in ascending order  
     coordinates.sort(key=lambda x: x.x, reverse=True)
     find_intersections(coordinates, number_of_points)
-    #print(answer)
-    
-    ## here goes the rest of code for analyzing etc
-    #print(f"{coordinates}")
