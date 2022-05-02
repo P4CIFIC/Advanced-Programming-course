ff3ada0088bf6bda425a2525528ff667033edf0e
P4CIFIC <malek_123_chawki@hotmail.com>
2022-05-02 16:31:34 +0200

    update

--- problems/15_10245/second_draft.py
+++ problems/15_10245/second_draft.py
@@ -71,8 +71,8 @@ class ClosestPair():
 
 
 lines = [line.rstrip('\n') for line in open(
-        r'C:\Users\malek\Desktop\Advanced-Programming-Assignments\problems\15_10245\super_testcases.txt')]
-        #r'C:\kurser\Advanced-Programming-Assignments\problems\15_10245\super_testcases.txt')]
+        #r'C:\Users\malek\Desktop\Advanced-Programming-Assignments\problems\15_10245\super_testcases.txt')]
+        r'C:\kurser\Advanced-Programming-Assignments\problems\15_10245\super_testcases.txt')]
 lines.pop()
 
 while True:
